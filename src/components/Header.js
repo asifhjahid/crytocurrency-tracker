2ca68ba1ac8a445277ea1bcf4b043c78ad5ad73e
asifhjahid <dhrubo8211diu@gmail.com>
2022-01-03 21:31:04 +0600

    complete banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import { AppBar, Container, makeStyles, MenuItem, Select, ThemeProvider, Toolbar
 import createTheme from '@material-ui/core/styles/createTheme';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { CryptoState } from '../context/CryptoContext';
 
 
 const useStyles = makeStyles(()=>({
@@ -17,6 +18,10 @@ const useStyles = makeStyles(()=>({
 export default function Header() {
     const classes = useStyles();
     const navigate = useNavigate();
+
+    const { currency, setCurrency } = CryptoState();
+    console.log(currency);
+
     const darkTheme = createTheme({
         palette:{
             primary:{
@@ -43,8 +48,13 @@ export default function Header() {
                      width:100,
                      height:40,
                      marginRight:15,
-                 }}>
-                     <MenuItem value={'UDS'}>USD</MenuItem>
+                 }}
+                 
+                 value={currency}
+                 onChange={(e)=>setCurrency(e.target.value)}
+
+                 >
+                     <MenuItem value={'USD'}>USD</MenuItem>
                      <MenuItem value={'BDT'}>BDT</MenuItem>
                  </Select>
              </Toolbar>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import CryptoContext from './context/CryptoContext';
 import './index.css';
 
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(
+        <CryptoContext>
+             <App />
+        </CryptoContext>,
+        
+        document.getElementById('root')
+);
 
